39276a58fc2c3e9269bdc63d41b9c86ddd40a89b
LynnL4 <lht856@foxmail.com>
2020-05-25 10:56:52 +0800

    add: add repl support

---
+++ repl/at_cmd_repl_freertos.h
@@ -0,0 +1,176 @@
+/* Edge Impulse ingestion SDK
+ * Copyright (c) 2020 EdgeImpulse Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _EDGE_IMPULSE_AT_COMMANDS_REPL_MBED_H_
+#define _EDGE_IMPULSE_AT_COMMANDS_REPL_MBED_H_
+
+#define EI_SIGNAL_TERMINATE_THREAD_REQ 0x1
+#define EI_SIGNAL_TERMINATE_THREAD_RES 0x4
+
+/**
+ * Mbed OS specific code for handling the REPL and throwing AT
+ * commands around...
+ */
+#include "at_cmds.h"
+#include "repl.h"
+#include "thread.hpp"
+#include "ticks.hpp"
+#include "message.hpp"
+#include "semaphore.hpp"
+
+using namespace cpp_freertos;
+
+typedef struct
+{
+    char cmd[256];
+} ei_at_mail_t;
+
+class AtCmdRepl : public Thread
+{
+public:
+    AtCmdRepl(SerialStream *serial)
+        : Thread(8*1024, 2),
+          _repl(serial),
+          Mail(256),
+          _mail_handled(1),
+          _terminate_thread(false)
+    {
+    }
+
+    /**
+     * Start the REPL
+     * (note that you'll need to run the event queue as well!)
+     */
+    void start_repl()
+    {
+        std::function<bool(const char *)> callback = std::bind(&AtCmdRepl::exec_command, this, placeholders::_1);
+        _repl.start(callback);
+        Start();
+    }
+
+    /**
+     * When executing a command we'll detach the UART interrupts from the REPL
+     * and handle them ourselves, to allow breaking off a command...
+     */
+    bool exec_command(const char *cmd)
+    {
+        if (strlen(cmd) > 255)
+        {
+            Serial.printf("Command is too long (max. length 255)\n");
+            return true;
+        }
+
+        _terminate_thread = false;
+        // wait for the semaphore to fire
+        _mail_handled.Take();
+        _repl.stop();
+        Mail.Send((void *)cmd, strlen(cmd));
+
+
+        // // event was cancelled
+        // if (_terminate_thread) {
+        //     osPriority_t priority = _cmd_thread->get_priority();
+
+        //     // signal the other thread, and wait until it's done
+        //     _cmd_thread->signal_set(EI_SIGNAL_TERMINATE_THREAD_REQ);
+
+        //     for (size_t ix = 0; ix < 10; ix++) {
+        //         ThisThread::sleep_for(200);
+        //         if (_cmd_thread->get_state() != Thread::State::Ready) {
+        //             break;
+        //         }
+        //     }
+
+        //     _cmd_thread->terminate();
+
+        //     delete _cmd_thread;
+
+        //     // have to restart the thread... have to make new one, because Mbed OS does not allow restarting terminated threads
+        //     _cmd_thread = new Thread(priority, _cmd_thread_stack_size, _cmd_thread_stack_mem, "at-cmd-thread");
+        //     _cmd_thread->start(callback(this, &AtCmdRepl::cmd_thread_main));
+        // }
+
+        // and command is handled, restart REPL
+        std::function<bool(const char *)> callback = std::bind(&AtCmdRepl::exec_command, this, placeholders::_1);
+        _repl.start(callback);
+
+        _terminate_thread = false;
+        return false; // don't reprint state (already done when starting the repl again)
+    }
+
+    void break_inference_loop_irq()
+    {
+        if (_repl.getSerial()->available() && _repl.getSerial()->read() == 'b')
+        {
+            _terminate_thread = true;
+
+            // release the semaphore so the main thread knows the event is done
+            _mail_handled.Give();
+        }
+    }
+
+    void reprintReplState()
+    {
+        _repl.reprintReplState();
+    }
+
+private:
+    /**
+     * Main thread for commands, pretty straight forward
+     * it will wait for an item in the mailbox, execute it, then use the semaphore to signal back that we're done
+     */
+    void Run()
+    {
+        while (1)
+        {
+            size_t nums = Mail.Receive(&buff, 256);
+            if (nums > 0)
+            {
+                //ei_at_cmd_handle(buff);
+                // Serial.printf("Receive: ");
+                // for (int i = 0; i < nums; i++)
+                // {
+                //     Serial.write(buff[i]);
+                // }
+                // Serial.printf(", length: %d\n\r", nums);
+                // signal to other thread
+                ei_at_cmd_handle((const char *)&buff);
+   
+                if (!_terminate_thread)
+                {
+                    // so this shouldn't be an issue I'd say but release'ing this again
+                    // (whilst it was also released in break_inference_loop_irq)
+                    // seems to break the state (and we can no longer aquire this semaphore later)
+                    _mail_handled.Give();
+                    //Serial.printf("Unlock\n\r");
+                }
+            }
+        }
+    }
+    Repl _repl;
+    Message Mail;
+    BinarySemaphore _mail_handled;
+    uint8_t buff[256];
+    bool _terminate_thread;
+};
+
+#endif // _EDGE_IMPULSE_AT_COMMANDS_REPL_MBED_H_
---
+++ repl/at_cmds.h
@@ -0,0 +1,41 @@
+/* Edge Impulse ingestion SDK
+ * Copyright (c) 2020 EdgeImpulse Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _EDGE_IMPULSE_AT_COMMANDS_CONFIG_H_
+#define _EDGE_IMPULSE_AT_COMMANDS_CONFIG_H_
+
+#include "at_cmd_interface.h"
+#include "at_base64.h"
+
+#define EDGE_IMPULSE_AT_COMMAND_VERSION        "1.3.0"
+
+static void at_reset() {
+    NVIC_SystemReset();
+}
+
+// AT commands related to configuration
+void ei_at_register_generic_cmds() {
+    ei_at_cmd_register("HELP", "Lists all commands", &ei_at_cmd_print_info);
+    ei_at_cmd_register("RESET", "Reset the system", &at_reset);
+}
+
+#endif // _EDGE_IMPULSE_AT_COMMANDS_CONFIG_H_
